7a950a122a18ab8cb9accb53ad4c2064c30e8046
Wu Zhangjin <wuzhangjin@gmail.com>
2015-07-09 01:36:58 +0800

    tools: mediawiki-2-markdown: Fix up more urls
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/mediawiki-2-markdown.sh
+++ tools/mediawiki-2-markdown.sh
@@ -35,13 +35,25 @@ cat ${page}.md | \
 #  * Remove the `Jump to` line
 #  * Remove the `From` line
 #  * Remove from `Navigation Menu` to the end
+#
+# Note:
+#  * pandoc: need drop / (.2f), gitbook: need convert it to -
+#  * pandoc/gitbook: need to drop :
+#  * gitbook: need to replace . with -
+#  * gitbook: need to replace "*" with -*
 
 cat ${page}.md | \
-    sed -e "/^## Content/,/^## Introduction/{s=](#\(.*\)=](#\L\1=g;s=_=-=g;s/.2f//g;s/.2c//g;s/.27//g;s/.5b//g;s/.5d//g;s/.28//g;s/.29//g;}" |\
+    sed -e "/^## Content/,/^## Introduction/{s=](#\(.*\)=](#\L\1=g;s=_=-=g;s/.2f/-/g;s/.2c//g;s/.27//g;s/.5b//g;s/.5d//g;s/.28//g;s/.29//g;}" |\
+    sed -e "/^## Content/,/^## Introduction/{s/(\([^:]*\):\(.*\))/(\1\2)/g;}" |\
+    sed -e "/^## Content/,/^## Introduction/{s/(#\([^\.]*\)\.22\([^\.]*\)\.22\(.*\))/(#-\1\2\3)/g;}" |\
+    sed -e "/^## Content/,/^## Introduction/{s/(\([^\.]*\)\.\(.*\))/(\1-\2)/g;}" |\
+    sed -e "/^## Content/,/^## Introduction/{s/enlightenment-foundation-libraries-efl/enlightenment-foundation-libraries-efl-http-elinux-org-efl-efl/g;}" |\
     sed -e "s=(/=(${target_site}/=g" |\
     sed -e "/^Jump to:/d" |\
     sed -e "/^From eLinux.org/d" |\
-    sed -e "/Navigation menu/,/\%$/d" > ${page}.md.tmp
+    sed -e "/Navigation menu/,/\%$/d" |\
+    sed -e "/Robot.*png.*ELC2013 Tech Zone Contest/,/^\* \* \* \* \*/d" |\
+    sed -e "/ELC2013 Tech Zone Contest/d" > ${page}.md.tmp
 
 # Insert From line with URL
 sed -i -e "1i> From: [$from](${site}/${page} \"${site}/${page}\")\n\n" ${page}.md.tmp
