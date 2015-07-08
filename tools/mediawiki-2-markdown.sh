c153495be0f07f0c63ffb41b68934c12b74b3fda
Wu Zhangjin <wuzhangjin@gmail.com>
2015-07-08 22:32:06 +0800

    tools: convert mediawiki to markdown and clean it up
    This script download a mediawiki page and convert it to markdown and at last
    clean it up.
    
    It use http://elinux.org/Boot_Time page as an example.
    
    To use it for the other mediawiki site, please modify the "from" variable in the script and then:
    
    $ ./mediawiki-2-markdown.sh page_name
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

---
+++ tools/mediawiki-2-markdown.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# Convert mediawiki html page to friendly markdown
+#
+# Author: Falcon <wuzhangjin@gmail.com>
+# From  : http://tinylab.org
+#         http://tinylab.gitbooks.io/elinux
+#
+
+page=$1
+[ -z "$page" ] && page=Boot_Time
+
+from=eLinux.org
+site=http://$from
+target_site=$site
+
+# Download the printable html page
+wget -O ${page}.html -c "${site}/index.php?title=${page}&printable=yes"
+
+# Convert to strict markdown file
+pandoc -f html -t markdown_strict --atx-headers ${page}.html > ${page}.md
+
+# Dump the url architecture
+cat ${page}.md | \
+    egrep "^#|\]\(/[A-Z]" |\
+    egrep -v "^$|^--$|/images/" |\
+    sed -e "/Navigation menu/,/\%$/d" |\
+    sed -e "s#.*](/\([^ ]*\) .*#\1#g" |\
+    egrep -v "\[[a-zA-Z]|[^#].*#|Category:|:Categories" | sed -e "s/^\([^#][^#]*\)/* \1/g" > ${page}.subpage.md
+
+# Misc Clean Up:
+#
+#  * Fix up internal links
+#  * Extend the urls
+#  * Remove the `Jump to` line
+#  * Remove the `From` line
+#  * Remove from `Navigation Menu` to the end
+
+cat ${page}.md | \
+    sed -e "/^## Content/,/^## Introduction/{s=](#\(.*\)=](#\L\1=g;s=_=-=g;s/.2f//g;s/.2c//g;s/.27//g}" |\
+    sed -e "s=(/=(${target_site}/=g" |\
+    sed -e "/^Jump to:/d" |\
+    sed -e "/^From eLinux.org/d" |\
+    sed -e "/Navigation menu/,/\%$/d" > ${page}.md.tmp
+
+# Insert From line with URL
+sed -i -e "1i> From: [$from](${site}/${page} \"${site}/${page}\")\n\n" ${page}.md.tmp
+
+mv ${page}.md.tmp ${page}.md
+
+# Convert back to html for validation
+pandoc -f markdown -t html ${page}.md > ${page}.new.html
