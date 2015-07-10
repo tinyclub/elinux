d08b43186c462ceeb43f282fc9bceac3b34e8508
Wu Zhangjin <falcon@meizu.com>
2015-07-10 19:31:11 +0800

    tools: Fix up the toc handling
    The tools have broken the pages without Introduction sections, this fix up it.
    
    Signed-off-by: Wu Zhangjin <falcon@meizu.com>

--- tools/mediawiki-2-markdown.sh
+++ tools/mediawiki-2-markdown.sh
@@ -47,13 +47,13 @@ cat ${page}.md | \
 #  * the handling of .xx is wrong, need to build toc automatically instead of ugly workarounds.
 
 cat ${page}.md | \
-    sed -e "/^## Content/,/^## Introduction/{s=](#\(.*\)=](#\L\1=g;s=_=-=g;s/.2f/-/g;s/.2c/-/g;s/.27/-/g;s/.5b/-/g;s/.5d/-/g;s/.2b/-/g;s/.28/-/g;s/.29/-/g;s/.7c/-/g;s/.7b/-/g;s/.7d//g;s/--/-/g}" |\
-    sed -e "/^## Content/,/^## Introduction/{s/(\([^:]*\):\(.*\))/(\1\2)/g;}" |\
-    sed -e "/^## Content/,/^## Introduction/{s/(#\([^\.]*\)\.22\([^\.]*\)\.22\(.*\))/(#-\1\2\3)/g;}" |\
-    sed -e "/^## Content/,/^## Introduction/{s/(\([^\.]*\)\.\(.*\))/(\1-\2)/g;}" |\
-    sed -e "/^## Content/,/^## Introduction/{s/--/-/g;s/-)/)/g;s/(#-/(#/g;s/-\./-/g;s/--/-/g;s/--/-/g;}" |\
-    sed -e "/^## Content/,/^## Introduction/{s/(\([^(]*\)\./(\1-/g;}" |\
-    sed -e "/^## Content/,/^## Introduction/{s/enlightenment-foundation-libraries-efl/enlightenment-foundation-libraries-efl-http-elinux-org-efl-efl/g;}" |\
+    sed -e "/^## Content/,/^##* /{s=](#\(.*\)=](#\L\1=g;s=_=-=g;s/\.2f/-/g;s/\.2c/-/g;s/\.27/-/g;s/\.5b/-/g;s/\.5d/-/g;s/\.2b/-/g;s/\.28/-/g;s/\.29/-/g;s/\.7c/-/g;s/\.7b/-/g;s/\.7d//g;s/--/-/g;}" |\
+    sed -e "/^## Content/,/^##* /{s/(\([^:]*\):\(.*\))/(\1\2)/g;}" |\
+    sed -e "/^## Content/,/^##* /{s/(#\([^\.]*\)\.22\([^\.]*\)\.22\(.*\))/(#-\1\2\3)/g;}" |\
+    sed -e "/^## Content/,/^##* /{s/(\([^\.]*\)\.\(.*\))/(\1-\2)/g;}" |\
+    sed -e "/^## Content/,/^##* /{s/(\([^(]*\)\./(\1-/g;}" |\
+    sed -e "/^## Content/,/^##* /{s/---/-/g;s/--/-/g;s/-)/)/g;s/-\./-/g;s/---/-/g;s/--/-/g;s/-)/)/g;}" |\
+    sed -e "/^## Content/,/^##* /{s/enlightenment-foundation-libraries-efl/enlightenment-foundation-libraries-efl-http-elinux-org-efl-efl/g;}" |\
     sed -e "s=(/=(${target_site}/=g" |\
     sed -e "/^Jump to:/d" |\
     sed -e "/^From eLinux.org/d" |\
@@ -61,6 +61,9 @@ cat ${page}.md | \
     sed -e "/Robot.*png.*ELC2013 Tech Zone Contest/,/^\* \* \* \* \*/d" |\
     sed -e "/ELC2013 Tech Zone Contest/d" |\
     sed -e "/^Retrieved from/,/oldid=[0-9][0-9]*/d" |\
+    sed -e "s# href=\"/# href=\"http://elinux.org/#g" |\
+    sed -e "s# src=\"/# src=\"http://elinux.org/#g" |\
+    sed -e "s#/Category::#/Category:#g" |\
     sed -e "s/[[:space:]][[:space:]]*$//g" |\
     sed -e "/^[[:space:]][[:space:]]*$/d" > ${page}.md.tmp
 
