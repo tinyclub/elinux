2d6e366ee59f4c833e311e8dcafdc58ca90fb17c
Wu Zhangjin <wuzhangjin@gmail.com>
2015-10-23 10:15:27 +0800

    tools: Update the build-contributors.sh
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/build-contributors.sh
+++ tools/build-contributors.sh
@@ -10,7 +10,7 @@ do
 	sed -e "/^<hr>/q" < ${lang}/README.md > ${lang}/README.tmp.md
 	grep ">.*\[@" -ur zh/ | grep "翻译" | cut -d ">" -f2 | sed -e "s/.*\(\[.*)\).*/\1/g" \
 		| sort | uniq -c | grep "\[@" \
-		| sort -k1 -r | tr -s ' ' | sed -e "s/ \([0-9]*\) \(.*\)/\2 (\1)/g" \
+		| sort -k1 -r -g | tr -s ' ' | sed -e "s/ \([0-9]*\) \(.*\)/\2 (\1)/g" \
 		| xargs -i echo "-   " {} "<br/>" >> ${lang}/README.tmp.md
 	mv ${lang}/README.tmp.md ${lang}/README.md
 done
